c798864fb1dc1d343838b3742c0ef4138003eb09
lcvf92 <lcvf92@gmail.com>
2017-09-23 19:49:20 -0300

    cambios index

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is M.Reza", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["My Name is Luis Valdés", "I'm a software developer", "Love peace"],
         typeSpeed: 100,
         backDelay: 900,
         // loop
